9324ebeef4155a84d44fca459c352702269e6918
Sinethemba-15 <Hope4685@gmail.com>
2025-08-10 19:44:07 +0200

    added basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function generateQuote(event) {
+  event.preventDefault();
+  new Typewriter("#quote", {
+    strings: "The quote starts here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
